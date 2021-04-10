4a76a3426ed686bf5846a0afa64e9ab966bc4785
Charlie-Booth <charliebtf@outlook.com>
2021-04-10 17:07:08 +0100

    Spinning Enemies

---
+++ BulletHell Game/Assets/Rotate.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, 50 * Time.deltaTime); //rotates 50 degrees per second around z axis
+    }
+}
