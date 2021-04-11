41fd9e018561731415acff81c4cb0eb3a4952f4b
Toooble <tobiclement83@gmail.com>
2021-04-11 13:26:30 +0100

    Minor Options changes

--- BulletHell Game/Assets/Scripts/Menus/OptionsScript.cs
+++ BulletHell Game/Assets/Scripts/Menus/OptionsScript.cs
@@ -2,14 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.Rendering.PostProcessing;
 
 public class OptionsScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public PostProcessVolume volume;
     public AudioMixer audioMixer;
 
+    //private CGM cgm;
+
+    void Start()
+    {
+        //volume.profile.TryGetSettings(out cgm);
+    }
     public void Volume(float vol)
     {
         audioMixer.SetFloat("Volume", vol);
     }
+
+   // public void Brightness(float brightness)
+   // {
+        //Target brightness
+   // }
 }
