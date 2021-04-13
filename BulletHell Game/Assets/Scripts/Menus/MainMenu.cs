7e270e30f876cf6bd9e3524c1ebac9a6ee770caa
Torterrad <64802272+Torterrad@users.noreply.github.com>
2021-04-13 01:55:23 +0100

    Scene Transition

--- BulletHell Game/Assets/Scripts/Menus/MainMenu.cs
+++ BulletHell Game/Assets/Scripts/Menus/MainMenu.cs
@@ -5,13 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public Animator transition;
+
+    public float transitionTime;
+
     public void Play()
     {
-        SceneManager.LoadScene("MainGame");
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
     }
 
     public void Quit()
     {
         Application.Quit();
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
     }
 }
