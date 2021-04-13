02edf9fcafa356aba25f8a0db2ae783ee3e0785a
Charlie-Booth <charliebtf@outlook.com>
2021-04-13 04:11:05 +0100

    ParticleEffect and slight screen shake when passing through a shape

--- BulletHell Game/Assets/Scripts/EnemyController.cs
+++ BulletHell Game/Assets/Scripts/EnemyController.cs
@@ -8,6 +8,8 @@ public class EnemyController : MonoBehaviour
     GameObject manager;
     
     public int dmg;
+
+    public ParticleSystem passThrough;
     void Start()
     {
         player = GameObject.Find("Player");
@@ -43,22 +45,29 @@ public class EnemyController : MonoBehaviour
 
             if (this.tag == "BlueEnemy" && other.GetComponent<PlayerController>().isBlue == true)
             {
+                ScreenShakeController.instance.StartShake(.2f, .25f);
                 manager.GetComponent<ScoreSystem>().score += 10 * manager.GetComponent<ScoreSystem>().multiplier;
                 //other.GetComponent<PlayerController>().gravAmount += other.GetComponent<PlayerController>().gravIncrease;
             }
             if (this.tag == "GreenEnemy" && other.GetComponent<PlayerController>().isGreen == true)
             {
+                ScreenShakeController.instance.StartShake(.2f, .25f);
                 manager.GetComponent<ScoreSystem>().score += 10 * manager.GetComponent<ScoreSystem>().multiplier;
                 //other.GetComponent<PlayerController>().gravAmount += other.GetComponent<PlayerController>().gravIncrease;
             }
             if (this.tag == "RedEnemy" && other.GetComponent<PlayerController>().isRed == true)
             {
+                ScreenShakeController.instance.StartShake(.2f, .25f);
                 manager.GetComponent<ScoreSystem>().score += 10 * manager.GetComponent<ScoreSystem>().multiplier;
                 //other.GetComponent<PlayerController>().gravAmount += other.GetComponent<PlayerController>().gravIncrease;
             }
         }
         
     }
+
+    
+    
+
     private void OnTriggerStay2D(Collider2D other)
     {
         if (other.transform.tag == "Player")
@@ -66,18 +75,39 @@ public class EnemyController : MonoBehaviour
             if (this.tag == "BlueEnemy" && other.GetComponent<PlayerController>().isBlue == true)
             {
                 
+                passThrough.Play();
                 other.GetComponent<PlayerController>().gravAmount += other.GetComponent<PlayerController>().gravIncrease;
             }
             if (this.tag == "GreenEnemy" && other.GetComponent<PlayerController>().isGreen == true)
             {
                 
+                passThrough.Play();
                 other.GetComponent<PlayerController>().gravAmount += other.GetComponent<PlayerController>().gravIncrease;
             }
             if (this.tag == "RedEnemy" && other.GetComponent<PlayerController>().isRed == true)
             {
                 
+                passThrough.Play();
                 other.GetComponent<PlayerController>().gravAmount += other.GetComponent<PlayerController>().gravIncrease;
             }
+            passThrough.Play();
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (this.tag == "BlueEnemy" && other.GetComponent<PlayerController>().isBlue == true)
+        {
+            passThrough.Stop();
+        }
+        if (this.tag == "GreenEnemy" && other.GetComponent<PlayerController>().isGreen == true)
+        {
+            passThrough.Stop();
+        }
+        if (this.tag == "RedEnemy" && other.GetComponent<PlayerController>().isRed == true)
+        {
+            passThrough.Stop();
         }
     }
 }
